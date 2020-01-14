72dbd155de7401d5d1a9dd081d7d117048cd69b0
Arnar Ingi Gunnarsson <23302456+arnaringig@users.noreply.github.com>
2020-01-14 23:36:34 +0000

    Create fast_power.py

---
+++ python/datastructures2020/fast_power.py
@@ -0,0 +1,15 @@
+# A log(n) implementation of the power function
+def fast_power(base,power,result):
+	if power <= 1:
+		result = result * base
+		return result
+	if power % 2 == 0:
+		base = base * base
+		power = power // 2
+		return fp(base,power,result)
+	else:
+		power -= 1
+		result = result * base
+		power = power // 2
+		base = base * base
+		return fp(base,power,result)
